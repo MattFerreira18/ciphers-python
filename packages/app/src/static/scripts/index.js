08b473938092a1e575a39ea74416300980e064d1
MattFerreira18 <matheusferreira.dev@gmail.com>
2022-10-22 18:07:05 -0300

    feat: create initial styles for the pages

--- packages/app/src/static/scripts/index.js
+++ packages/app/src/static/scripts/index.js
@@ -1,3 +1,11 @@
+const PAGE_PATHS = {
+  HOME: "/",
+  ERROR: "/500",
+  RESULT: "/result",
+};
+
+const toResultPageHref = (param) => `${PAGE_PATHS.RESULT}/${param}`;
+
 function main() {}
 
 main();
